9c4354cdcdd00c910d92ce014b81bed1ec5c34ba
dkrayacich <david.krayacich@gmail.com>
2024-03-15 19:31:14 -0400

    commit

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def main():
     right_side_speed = 0
     while True:
 
-        if rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
-            while rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
+        if rover.laser_distances[28] > 0.5 and rover.laser_distances[29] > 0.5 and rover.laser_distances[30] > 0.5:
+            while rover.laser_distances[28] > 0.5 and rover.laser_distances[29] > 0.5 and rover.laser_distances[30] > 0.5:
                 left_side_speed = 1
                 right_side_speed = -1
                 rover.send_command(left_side_speed, right_side_speed)
 
 
-        if rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5 and rover.laser_distances[10] < 2:
-            while rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5:
+        if rover.laser_distances[24] < 0.5 and rover.laser_distances[25] < 0.5 and rover.laser_distances[26] < 0.5 and rover.laser_distances[15] < 2:
+            while rover.laser_distances[24] < 0.5 and rover.laser_distances[25] < 0.5 and rover.laser_distances[26] < 0.5 and rover.laser_distances[15] < 2:
                 left_side_speed = -1
                 right_side_speed = 1
                 rover.send_command(left_side_speed, right_side_speed)
