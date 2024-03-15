eb022596e491630670f980ed679e7d5656a00144
dkrayacich <david.krayacich@gmail.com>
2024-03-15 19:20:49 -0400

    commit

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-#test
-print("Hello World")
+from qset_lib import Rover, AngleReader
+from time import sleep
+import signal
+
+def main():
+    rover = Rover() # this line starts the connection to the rover and gives access to the rover data
+    angle_reader = AngleReader()
+
+
+    left_side_speed = 0
+    right_side_speed = 0
+    while True:
+
+        if rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
+            while rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
+                left_side_speed = 1
+                right_side_speed = -1
+                rover.send_command(left_side_speed, right_side_speed)
+
+
+        if rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5 and rover.laser_distances[10] < 2:
+            while rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5:
+                left_side_speed = -1
+                right_side_speed = 1
+                rover.send_command(left_side_speed, right_side_speed)
+
