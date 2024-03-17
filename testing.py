2abf14e61995d6420489ef1f1060482cefcd9ae2
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-17 12:46:42 -0400

    o

--- testing.py
+++ testing.py
@@ -6,11 +6,10 @@ def main():
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
     angle_reader = AngleReader()
 
+    # Main program loop
     while True :
-        i = 0
         for dist in rover.laser_distances:
-            print(dist[i])
-            i += 1
+            print(rover.laser__distances[dist])
 
 if __name__ == "__main__":
     main()
