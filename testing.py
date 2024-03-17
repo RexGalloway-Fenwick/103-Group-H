999bfc17c28325105276dcfd5919dd7cea7a641f
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-17 12:42:55 -0400

    testing file update

--- testing.py
+++ testing.py
@@ -2,11 +2,15 @@ from qset_lib import Rover, AngleReader
 from time import sleep
 import signal
 
-rover = Rover() # this line starts the connection to the rover and gives access to the rover data
-angle_reader = AngleReader()
+def main():
+    rover = Rover() # this line starts the connection to the rover and gives access to the rover data
+    angle_reader = AngleReader()
 
-while True :
-    i = 0
-    for dist in rover.laser_distances:
-        print(dist[i])
-        i += 1
+    while True :
+        i = 0
+        for dist in rover.laser_distances:
+            print(dist[i])
+            i += 1
+
+if __name__ == "__main__":
+    main()
