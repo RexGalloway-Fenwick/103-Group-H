1fa9e5cac1fbf501d3750e3488b06b4cbd8e0ddc
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-17 12:51:54 -0400

    o

--- testing.py
+++ testing.py
@@ -1,15 +1,61 @@
 from qset_lib import Rover, AngleReader
 from time import sleep
 import signal
+import numpy
+import math
 
 def main():
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
     angle_reader = AngleReader()
 
-    # Main program loop
-    while True :
-        for dist in rover.laser_distances:
-            print(rover.laser_distances[dist])
+    # This line allows for the KeyboardInterrupt to worl
+    signal.signal(signal.SIGINT, signal.default_int_handler)
+    i = 0
+
+    desiredX = 10
+    desiredY = 10
+    desiredAngle = numpy.arctan(desiredX/desiredY)*-1*180/math.pi
+    print(desiredAngle)
+    while True:
+        i = 0
+        i += 1
+
+    while rover.heading - desiredAngle > 10 or rover.heading - desiredAngle < -10:
+        print(angle_reader.read_angle - desiredAngle)
+        left_side_speed = 1
+        right_side_speed = -1
+        rover.send_command(left_side_speed, right_side_speed)
+
+    
+    left_side_speed = 1
+    right_side_speed = 1
+    rover.send_command(left_side_speed, right_side_speed)
+    
+
+    isTooClose = False
+    try:
+        while not isTooClose or True:
+            print(" ANGLE: " + str(angle_reader.read_angle))
+            # this line prints the current location and heading of the rover
+            print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
+            # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.5 meters
+            for dist in rover.laser_distances:
+                print(dist)
+                if dist < 0.5:
+                    print("TOO CLOSE")
+                    left_side_speed = 0
+                    right_side_speed = 0
+                    isTooClose = True
+                    break
+            # the below line sends a command to the rover (simulation) 
+            rover.send_command(left_side_speed, right_side_speed)
+            i = i + 1
+            sleep(1)
+
+    except KeyboardInterrupt:
+        pass
+
+    rover.send_command(0, 0)
 
 if __name__ == "__main__":
     main()
