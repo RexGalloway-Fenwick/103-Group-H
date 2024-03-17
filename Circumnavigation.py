859f20c6de6c46b14d143d794d977a2fc3e8e030
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-17 19:01:08 -0400

    uf

--- Circumnavigation.py
+++ Circumnavigation.py
@@ -3,31 +3,33 @@ from time import sleep
 import signal
 
 def main():
-    right_side_speed = 1
-    left_side_speed = 1
-
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
     angle_reader = AngleReader()
 
     # This line allows for the KeyboardInterrupt to worl
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
-    while True:
-        # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.5 meters
-        for dist in rover.laser_distances:
-            # print(dist)
-            if dist < 0.5:
-                #circumnavigate()
-                print("TOO CLOSE")
-                right_side_speed = 1
-                left_side_speed = -1
-                if all(rover.laser_distances[1:29] > 5):
-                    right_side_speed = 0
-                    left_side_speed = 0
+    right_side_speed = 1
+    left_side_speed = 1
+    try:
+        while True:
+            # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.5 meters
+            for dist in rover.laser_distances:
+                # print(dist)
+                if dist < 0.5:
+                    print("TOO CLOSE")
+                    right_side_speed = 1
+                    left_side_speed = -1
+                    for x in range(0,28):
+                        if rover.laser_distances[x] == 2:
+                            right_side_speed = 0
+                            left_side_speed = 0
 
-        # the below line sends a command to the rover (simulation) 
-        rover.send_command(left_side_speed, right_side_speed)
-        sleep(0.01)
+            # the below line sends a command to the rover (simulation) 
+            rover.send_command(left_side_speed, right_side_speed)
+            sleep(0.01)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
