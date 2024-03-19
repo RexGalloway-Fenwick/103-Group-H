327a58819a39ed8d1e571b6d714b0e41bdffb6f7
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-19 12:17:14 -0400

    Can now access specific elements

--- Circumnavigation.py
+++ Circumnavigation.py
@@ -9,8 +9,11 @@ def main():
     # This line allows for the KeyboardInterrupt to worl
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
+    #Variable declaration
     right_side_speed = 1
     left_side_speed = 1
+    rover.send_command(left_side_speed, right_side_speed)
+
 
     try:
         while True:
@@ -22,13 +25,9 @@ def main():
                     right_side_speed = 1
                     left_side_speed = -1
                     break
-            print(rover.laser_distances)
-            print(rover.laser_distances)
-            print("\n")
-            print(len(rover.laser_distances))
-            print("\n")
-            print(rover.laser_distances) #TODO WHAT THE FUCK
-
+            #Make sure the rover.laser_distances is initialised
+            if len(rover.laser_distances) > 0:
+                print(rover.laser_distances[0])
 
             # the below line sends a command to the rover (simulation) 
             rover.send_command(left_side_speed, right_side_speed)
@@ -36,6 +35,7 @@ def main():
     except KeyboardInterrupt:
         pass
 
+    #stop the rover's movement when the program ends
     rover.send_command(0, 0)
 
 
