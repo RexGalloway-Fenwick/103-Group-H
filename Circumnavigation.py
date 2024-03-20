6c842b5c97ff585d55c163f8a11e49804312f843
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-20 12:22:53 -0400

    Cleaned up code

--- Circumnavigation.py
+++ Circumnavigation.py
@@ -10,24 +10,28 @@ def main():
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
     #Variable declaration
-    right_side_speed = 1
-    left_side_speed = 1
-    rover.send_command(left_side_speed, right_side_speed)
+    print("?????")
+    rover.send_command(1, 1)
 
 
     try:
-        #Main loop
+        tooclose = False
         while True:
             # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.5 meters
             for dist in rover.laser_distances:
                 # print(dist)
                 if dist < 0.5:
                     print("TOO CLOSE")
-                    right_side_speed = 1
-                    left_side_speed = -1
+                    tooclose = True
                     break
-                
-            # Make sure the laser distances array is populated
+            if(tooclose):
+                break
+            rover.send_command(2, 2)
+            sleep(0.01)
+
+        stopTurning = False
+        while True:        
+        # Make sure the laser distances array is populated
             if len(rover.laser_distances) > 0: 
                 #Stop if the last few lasers are the only ones that register the obstacle (check the last few for consistency)
                 for i in range(28, 1 , -1): #Lasers 1-28 are the ones that work
@@ -35,12 +39,15 @@ def main():
                     if rover.laser_distances[i] < 5:
                         break
                     if i == 3: 
-                        right_side_speed = 1
-                        left_side_speed = 1
+                        rover.send_command(0, 0)
+                        stopTurning = True
 
-            # the below line sends a command to the rover (simulation) 
-            rover.send_command(left_side_speed, right_side_speed)
-            sleep(0.01)
+                # the below line sends a command to the rover (simulation) 
+                rover.send_command(-1, 1)
+                sleep(0.01)
+            if stopTurning:
+                break
+
     except KeyboardInterrupt:
         pass
 
