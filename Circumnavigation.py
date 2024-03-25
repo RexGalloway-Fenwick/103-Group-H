dc4f01e76d6ba0ce94ffd3f2155a580f13cdde19
RexGalloway-Fenwick <22bvlb@queensu.ca>
2024-03-25 14:00:20 -0400

    fully circumnavigates

--- Circumnavigation.py
+++ Circumnavigation.py
@@ -10,11 +10,29 @@ def perpCheck():
     if len(rover.laser_distances) > 0: 
         #Stop if the last few lasers are the only ones that register the obstacle (check the last few for consistency)
         for i in range(28, 1 , -1): #Lasers 1-28 are the ones that work
+            print(i)
             if rover.laser_distances[i] < 1:
-                break
+                return False
             if i == 3: 
-                rover.send_command(0, 0)
+                if rover.laser_distances[2] < 2:
+                    return True
+                else: 
+                    return False
+            
+
+def tooFarLeft():
+    if len(rover.laser_distances) > 0:
+        for i in range(1, 28, 1): #Lasers 1-28 are the ones that work
+            if rover.laser_distances[i] < 2:
+                return False
+            if i == 27: 
                 return True
+            
+def turnRight():
+    while True:
+        rover.send_command(1, -1)
+        if perpCheck():
+            return
             
 def distCheck(distance):
     for dist in rover.laser_distances: 
@@ -24,19 +42,36 @@ def distCheck(distance):
         
 def circumnavigate():
     #start circumnavigating by getting parallel to the obstacle
-    while True:        
+    while True:       
+            #returns true when perpendicular, which breaks out of the loop 
             if perpCheck():
                 break
             rover.send_command(-1, 1) #Turn left
             sleep(0.01)
     while True:
-        rover.send_command(2, 2)
+        rover.send_command(2,2)
+
+        if tooFarLeft():
+            sleep(3) # Give the rover time to fully pass the object before turning
+            while True:
+                rover.send_command(1, -1)
+                if perpCheck():
+                    break
+                sleep(0.01)
+        
+        #Check here to see if we are back on the line
+                
+        #Check to see if we've done a full 360, if so create a new line to the destination and restart the
+        #program on that line
+
+        #If we're too close to an object we start again
         if distCheck(0.5):
             circumnavigate()
+        sleep(0.01)
     return
 
 def main():
-    # This line allows for the KeyboardInterrupt to worl
+    # This line allows for the KeyboardInterrupt to work
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
     #Variable declaration
